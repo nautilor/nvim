faf0ef014f7d97134d0ac71b432b48ac97744bbd
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2024-11-05 19:44:29 +0100

    removed lazygit shortcut

--- lua/shadow/remap.lua
+++ lua/shadow/remap.lua
@@ -3,6 +3,5 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<tab>", vim.cmd.bnext)
 vim.keymap.set("n", "<S-tab>", vim.cmd.bprevious)
 vim.keymap.set("n", "<leader>bd", ":bdelete!<CR>")
-vim.keymap.set("n", "<leader>gg", ":edit term://lazygit<CR> i")
 vim.keymap.set("n", "<leader>tt", ":edit term://zsh<CR> i")
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
