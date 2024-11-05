0d3583434f59e43911493bbc46190c93f202f2be
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2024-11-05 20:45:16 +0100

    vim as global for lua language server

--- lua/shadow/plugins/nvim-lspconig.lua
+++ lua/shadow/plugins/nvim-lspconig.lua
@@ -1,17 +1,24 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = { 'saghen/blink.cmp' },
-  config = function(_, opts)
-    local lspconfig = require('lspconfig')
-    local blink = require("blink.cmp")
-    lspconfig.pyright.setup({
-	    capabilities = blink.get_lsp_capabilities(lspconfig.pyright.capabilities)
-    })
-    lspconfig.eslint.setup({
-    capabilities = blink.get_lsp_capabilities(lspconfig.eslint.capabilities)
-    })
-		lspconfig.lua_ls.setup({
-    capabilities = blink.get_lsp_capabilities(lspconfig.lua_ls.capabilities)
+	'neovim/nvim-lspconfig',
+	dependencies = { 'saghen/blink.cmp' },
+	config = function(_, opts)
+		local lspconfig = require('lspconfig')
+		local blink = require("blink.cmp")
+		lspconfig.pyright.setup({
+			capabilities = blink.get_lsp_capabilities(lspconfig.pyright.capabilities)
 		})
-  end
+		lspconfig.eslint.setup({
+			capabilities = blink.get_lsp_capabilities(lspconfig.eslint.capabilities)
+		})
+		lspconfig.lua_ls.setup({
+			capabilities = blink.get_lsp_capabilities(lspconfig.lua_ls.capabilities),
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { 'vim' }
+					}
+				}
+			}
+		})
+	end
 }
