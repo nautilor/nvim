80fe204999bce386095a01ef5092a22d3811b2eb
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2024-11-12 03:54:35 +0100

    onedark to gruvbox

--- lua/shadow/plugins/colorscheme.lua
+++ lua/shadow/plugins/colorscheme.lua
@@ -1,10 +1,15 @@
 return {
-	'navarasu/onedark.nvim',
+	"ellisonleao/gruvbox.nvim",
 	-- use darker version
 	opts = { style = 'darker' },
+	init = function()
+		vim.g.gruvbox_contrast_dark = 'hard'
+		vim.cmd([[ set background=dark ]])
+		vim.cmd([[ colorscheme gruvbox ]])
+	end,
 	-- set colorscheme
 	config = function(_, opts)
-		require("onedark").setup(opts)
-		vim.cmd([[ colorscheme onedark]])
+		require("gruvbox").setup(opts)
+		vim.cmd([[ colorscheme gruvbox]])
 	end
 }
--- lua/shadow/plugins/lualine.lua
+++ lua/shadow/plugins/lualine.lua
@@ -3,16 +3,16 @@ return {
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
 		options = {
-			theme = "onedark",
-			section_separators = { left = '', right = '' },
+			theme = "gruvbox",
+			--section_separators = { left = '', right = '' },
 		},
 		sections = {
-			lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+			lualine_a = { { 'mode', } }, --separator = { left = '' }, right_padding = 2 } },
 			lualine_b = { 'branch', 'diff', 'diagnostics' },
 			lualine_c = { 'filename' },
 			lualine_x = { 'filetype' },
 			lualine_y = { 'progress' },
-			lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } }
+			lualine_z = { { 'location', } } -- separator = { right = '' }, left_padding = 2 } }
 		},
 		tabline = {
 			lualine_a = {
