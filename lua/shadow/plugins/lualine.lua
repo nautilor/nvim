cf200c973e5f7387dfccb2ee45f81f1128d2a989
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2024-11-25 01:25:01 +0100

    lualine fix + removed noice pop up notification

--- lua/shadow/plugins/lualine.lua
+++ lua/shadow/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
 			--section_separators = { left = '', right = '' },
 		},
 		sections = {
-			lualine_a = { { 'mode', } }, --separator = { left = '' }, right_padding = 2 } },
+			lualine_a = { { 'mode', } }, -- separator = { left = '' }, right_padding = 2 } },
 			lualine_b = { 'branch', 'diff', 'diagnostics' },
 			lualine_c = { 'filename' },
 			lualine_x = { 'filetype' },
