f2b831b18d4f6907d1b489e75b5bfbecfae6227d
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2024-12-05 22:50:15 +0100

    fixed some keybindings

--- lua/shadow/plugins/alpha.lua
+++ lua/shadow/plugins/alpha.lua
@@ -25,11 +25,11 @@ return {
 
 		}
 		dashboard.section.buttons.val = {
-			dashboard.button("n", "  > New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("e", "  > File Manager", ":NvimTreeToggle<CR>"),
-			dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
-			dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-			dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+			dashboard.button("n", "  > New file", "<cmd>ene <BAR> startinsert <CR>"),
+			dashboard.button("e", "  > File Manager", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("f", "  > Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("r", "  > Recent", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
 		}
 		local fortune = require("alpha.fortune")
 		dashboard.section.footer.val = fortune()
--- lua/shadow/plugins/gitsigns.lua
+++ lua/shadow/plugins/gitsigns.lua
@@ -4,6 +4,6 @@ return {
 		current_line_blame = true
 	},
 	init = function()
-		vim.keymap.set("n", "<leader>gp", ":Gitsign preview_hunk<CR>", {})
+		vim.keymap.set("n", "<leader>gp", "<cmd>Gitsign preview_hunk<CR>", {})
 	end
 }
