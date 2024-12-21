805770e727f6cfadacd88c12ef920319c7b32ecb
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2024-12-21 02:06:22 +0100

    fixed blink.cmp

--- lua/shadow/plugins/blink.lua
+++ lua/shadow/plugins/blink.lua
@@ -2,11 +2,25 @@ return {
 	"saghen/blink.cmp",
 	dependencies = { 'rafamadriz/friendly-snippets' },
 	event = { "LspAttach" },
-	version = 'v0.*',
+	version = '*',
 	opts = {
 		keymap = { preset = 'super-tab' },
-		highlight = {
-			use_nvim_cmp_as_default = true
+		signature = { enabled = true },
+		completion = {
+			menu = {
+				auto_show = true,
+				draw = {
+					columns = {
+						{ "label",     "label_description", gap = 1 },
+						{ "kind_icon", "kind" }
+					}
+				},
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+			},
+			ghost_text = { enabled = false },
 		}
 	}
 }
