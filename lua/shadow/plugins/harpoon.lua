7141b41522a3593955d8d0d56f58925208b0bd8e
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2025-03-23 17:40:36 +0100

    removed unused plugins

--- lua/shadow/plugins/harpoon.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'ThePrimeagen/harpoon',
-	init = function()
-		vim.keymap.set("n", "<leader>hh", "<cmd>lua require('harpoon.mark').add_file()<CR>")
-		vim.keymap.set("n", "<leader>mm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>")
-		vim.keymap.set("n", "<leader>nn", "<cmd>lua require('harpoon.ui').nav_next()<CR>")
-		vim.keymap.set("n", "<leader>pp", "<cmd>lua require('harpoon.ui').nav_prev()<CR>")
-	end
-}
--- lua/shadow/plugins/precognition.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"tris203/precognition.nvim",
-	config = function()
-		require("precognition").toggle()
-	end,
-}
