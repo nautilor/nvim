567f1a351dd4f4ce0cf401e085d5d14ffa1391f7
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2025-03-30 20:28:56 +0200

    some cinnamon keybind breaks nvimtree

--- lua/shadow/plugins/cinnamon.lua
+++ lua/shadow/plugins/cinnamon.lua
@@ -14,13 +14,5 @@ return {
 		vim.keymap.set("n", "zb", function() cinnamon.scroll("zb") end)
 		vim.keymap.set("n", "gg", function() cinnamon.scroll("gg") end)
 		vim.keymap.set("n", "G", function() cinnamon.scroll("G") end)
-		vim.keymap.set({ "n", "x" }, "k", function() cinnamon.scroll("k", { count_only = true }) end)
-		vim.keymap.set({ "n", "x" }, "j", function() cinnamon.scroll("j", { count_only = true }) end)
-		vim.keymap.set({ "n", "x" }, "<Up>", function() cinnamon.scroll("<Up>", { count_only = true }) end)
-		vim.keymap.set({ "n", "x" }, "<Down>", function() cinnamon.scroll("<Down>", { count_only = true }) end)
-		vim.keymap.set({ "n", "x" }, "gk", function() cinnamon.scroll("gk", { count_only = true }) end)
-		vim.keymap.set({ "n", "x" }, "gj", function() cinnamon.scroll("gj", { count_only = true }) end)
-		vim.keymap.set({ "n", "x" }, "g<Up>", function() cinnamon.scroll("g<Up>", { count_only = true }) end)
-		vim.keymap.set({ "n", "x" }, "g<Down>", function() cinnamon.scroll("g<Down>", { count_only = true }) end)
 	end,
 }
