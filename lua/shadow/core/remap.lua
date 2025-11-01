d09700cbdc378c36cbf894589f4a43df613e6bb4
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2025-11-01 20:27:52 +0100

    reviewed keymap

--- lua/shadow/core/remap.lua
+++ lua/shadow/core/remap.lua
@@ -1,19 +1,31 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- navigate between buffers and close them
 vim.keymap.set("n", "<tab>", vim.cmd.bnext)
 vim.keymap.set("n", "<S-tab>", vim.cmd.bprevious)
-vim.keymap.set("n", "<leader>bd", "<cmd>bdelete!<CR>")
+vim.keymap.set("n", "<leader>bd", "<cmd>bd!<CR>")
+
+-- handle escape key while on terminal
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
-vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
 vim.keymap.set("n", "<leader>!", "<cmd>q!<CR>")
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>sv", "<cmd>source $MYVIMRC<CR>")
-vim.keymap.set("n", "<C-j>", "i")
-vim.keymap.set("n", "<C-y>", "\"+y")
-vim.keymap.set("n", "<C-p>", "\"+p")
-vim.keymap.set("v", "<C-y>", "\"+y")
-vim.keymap.set("v", "<C-p>", "\"+p")
+
+-- copy/paste to/from the system clipboard
+vim.keymap.set({ "n", "v" }, "<C-y>", "\"+y")
+vim.keymap.set({ "n", "v" }, "<C-p>", "\"+p")
+
+-- remove the keymap q and F1
 vim.keymap.set("n", "q", "")
-vim.keymap.set("n", "<F1>", "")
+vim.keymap.set({ "n", "i" }, "<F1>", "")
+
+-- reset the last search highlight
 vim.keymap.set("n", "<leader>/", "<cmd>nohlsearch<CR>")
+
+-- easier to move selected lines
+vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-Up>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<C-Left>", "<gv")
+vim.keymap.set("v", "<C-Right>", ">gv")
