044fa5f96312314d6760526317b411afddbf4353
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2025-11-12 01:43:40 +0100

    lualine theme

--- lua/shadow/plugins/lualine.lua
+++ lua/shadow/plugins/lualine.lua
@@ -1,3 +1,5 @@
+local mocha = require("catppuccin.palettes").get_palette "mocha"
+
 return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -5,10 +7,9 @@ return {
 		vim.opt.laststatus = 3
 	end,
 	opts = {
-
 		options = {
 			component_separators = { left = "", right = "" },
-			section_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
 			theme = 'catppuccin',
 		},
 		sections = {
@@ -20,11 +21,11 @@ return {
 						return vim.api.nvim_get_mode().mode == "t" and ""
 								or ""
 					end,
-					separator = { left = "" }
+					separator = { left = "" }
 				},
 				{
 					"mode",
-					separator = { right = "" }
+					separator = { right = "" }
 				}
 			},
 			lualine_b = {
@@ -40,9 +41,12 @@ return {
 			lualine_x = {
 			},
 			lualine_y = {
-				{ "filetype", colored = true, icon_only = true },
-				"filename",
-				'progress'
+				{
+					"filetype",
+					icon_only = true,
+				},
+				{ "filename", icon_only = true, },
+				{ "progress", icon_only = true, },
 			},
 			lualine_z = {
 				{
@@ -56,14 +60,16 @@ return {
 				},
 				{
 					"location",
-					separator = { right = "", left = "" }
+					fmt = function(location)
+						return location:gsub("%s+", "")
+					end,
+					separator = { right = "", left = "" }
 				},
 			},
 		},
 		inactive_sections = {
 			lualine_a = {
-				{ "filetype", colored = true, icon_only = true, icon = { align = "right" } },
-				"filename",
+				{ "filename" },
 			},
 			lualine_b = {
 			},
@@ -73,3 +79,23 @@ return {
 		},
 	}
 }
+
+-- 	rosewater = "#f5e0dc",
+-- 	flamingo = "#f2cdcd",
+-- 	pink = "#f5c2e7",
+-- 	mauve = "#cba6f7",
+-- 	red = "#f38ba8",
+-- 	maroon = "#eba0ac",
+-- 	peach = "#fab387",
+-- 	yellow = "#f9e2af",
+-- 	green = "#a6e3a1",
+-- 	teal = "#94e2d5",
+-- 	sky = "#89dceb",
+-- 	sapphire = "#74c7ec",
+-- 	blue = "#89b4fa",
+-- 	lavender = "#b4befe",
+-- 	text = "#cdd6f4",
+-- 	surface0 = "#313244",
+-- 	base = "#1e1e2e",
+-- 	mantle = "#181825",
+-- 	crust = "#11111b",
