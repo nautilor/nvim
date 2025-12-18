48469ff4577e872f42385ab0e88557bbf569c9ef
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2025-12-19 00:54:48 +0100

    global vim for lua_ls

--- lua/shadow/plugins/nvim-lspconig.lua
+++ lua/shadow/plugins/nvim-lspconig.lua
@@ -12,5 +12,13 @@ return {
 			"vscode_spring_boot_tools",
 			"copilot_language_server"
 		})
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" } }
+				}
+			}
+		})
 	end
 }
